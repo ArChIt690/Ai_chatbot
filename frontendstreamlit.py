47eaed808ed3a1b4b9f457f355bf21f77471b7cb
Archit chakraborty <architchakraborty47@gmail.com>
2026-07-23 02:52:08 +0530

    added streaming and added sidebar ui

--- frontendstreamlit.py
+++ frontendstreamlit.py
@@ -2,10 +2,17 @@ import streamlit as st
 from backend_ai import aibot
 from langchain_core.messages import HumanMessage
 
+#*********************************Session ui********************************************
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
-CONFIG = {"configurable" : {"thread_id" : "thread1"}}
 
+#*********************************sidebar ui********************************************
+st.sidebar.title('Ai Bot')
+st.sidebar.button('New Chat')
+st.sidebar.header('My conversations')
+
+
+#*********************************Main ui***********************************************
 for message in st.session_state['message_history']:
     with st.chat_message(message["role"]):
         st.text(message["content"])
@@ -17,8 +24,13 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = aibot.invoke({'messages' : [HumanMessage(content=user_input)]} , config=CONFIG)
-    ai_message = response['messages'][-1].content
-    st.session_state['message_history'].append({"role" : "assistant" , "content" : ai_message})
+
     with st.chat_message('assistant'):
-        st.text(ai_message)
+        ai_message = st.write_stream(
+        message_chunk.content for message_chunk, meta_data in aibot.stream(
+            {'messages' : [HumanMessage(content=user_input)]},
+            config = {"configurable" : {"thread_id" : "thread1"}},
+            stream_mode="messages",
+        )
+        )
+    st.session_state['message_history'].append({'role' : 'assistant', 'content' : ai_message})
